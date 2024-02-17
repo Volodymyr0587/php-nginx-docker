7a72228aaee8fb6b477aaf18c59030e1cc06bc9d
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-17 20:27:51 +0200

    Constructor Property Promotion; Nullsafe Operator

--- src/Transaction.php
+++ src/Transaction.php
@@ -4,36 +4,21 @@ declare(strict_types=1);
 
 class Transaction
 {
-    private float $amount;
-    private string $description;
+    private ?Customer $customer = null;
 
-    public function __construct(float $amount, string $description)
+    public function __construct(
+        private float $amount, 
+        private string $description
+    )
     {
-        $this->amount = $amount;
-        $this->description = $description;
     }
 
-    public function addTax(float $rate): Transaction
+    /**
+     * @return Customer|null
+     */
+    public function getCustomer(): ?Customer
     {
-        $this->amount += $this->amount * $rate / 100;
-
-        return $this;
+        return $this->customer;
     }
 
-    public function applyDiscount(float $rate): Transaction
-    {
-        $this->amount -= $this->amount * $rate / 100;
-
-        return $this;
-    }
-
-    public function getAmount(): float
-    {
-        return $this->amount;
-    }
-
-    public function __destruct()
-    {
-        echo 'Destruct ' . $this->description . '<br />';
-    }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -2,16 +2,13 @@
 
 declare(strict_types=1);
 
+require_once '../PaymentProfile.php';
+require_once '../Customer.php';
 require_once '../Transaction.php';
 
 // Classes & Objects
-$transaction = (new Transaction(100, 'Transaction 1'))
-    ->addTax(8)
-    ->applyDiscount(10);
+$transaction = new Transaction(5, 'Test');
 
-$amount = $transaction->getAmount();
+echo $transaction->getCustomer()?->getPaymentProfile()?->id ?? 'foo';
 
-unset($transaction);
-
-var_dump($amount);
 
